e44cd979efe3a3c3be72b6b52414efb920e89a61
Jennings Zhang <jenni_zh@protonmail.com>
2018-03-02 18:58:13 -0500

    Use jQuery to get around HTML5 security policy

--- note.js
+++ note.js
@@ -4,4 +4,10 @@ let note = '';
 if (document.location.search.includes('note'))
     note = new URLSearchParams(document.location.search.substring(1)).get('note');
 
-document.getElementById('note').innerHTML = note;
+/*
+ * stupid HTML5 security prevents script execution with innerHTML
+ * https://www.w3.org/TR/2008/WD-html5-20080610/dom.html#innerhtml0
+ * 
+ * alternative just to make this work is to use jQuery...
+ */
+$('#note').html(note);

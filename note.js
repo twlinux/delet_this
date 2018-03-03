b3d24981f19d8a87463a3c9f6124ac67112bafab
Jennings Zhang <jenni_zh@protonmail.com>
2018-03-02 19:30:08 -0500

    Add stuff for docs

--- note.js
+++ note.js
@@ -4,10 +4,4 @@ let note = '';
 if (document.location.search.includes('note'))
     note = new URLSearchParams(document.location.search.substring(1)).get('note');
 
-/*
- * stupid HTML5 security prevents script execution with innerHTML
- * https://www.w3.org/TR/2008/WD-html5-20080610/dom.html#innerhtml0
- * 
- * alternative just to make this work is to use jQuery...
- */
 $('#note').html(note);
